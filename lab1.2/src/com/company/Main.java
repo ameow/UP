1db157d7771e034a9002508e51e5d2d6324ea58d
Anastasia <anastasiameoww@gmail.com>
2016-03-01 13:00:05 +0300

    lab3_html

--- lab1.2/src/com/company/Main.java
+++ lab1.2/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         try {
             FileWriter writer = new FileWriter("Output.json", false);
             writer.close();
-             FileWriter writer1 = new FileWriter("logfile.txt", false);
+            FileWriter writer1 = new FileWriter("logfile.txt", false);
             writer1.close();
             Transaction transaction = new Transaction();
             transaction.start();
