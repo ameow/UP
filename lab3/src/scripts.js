bcb920fd77cfc16d0fefb83f15dc754bf5700a9b
Anastasia <anastasiameoww@gmail.com>
2016-03-18 16:34:54 +0300

    [lab5] java script. Без логирования.

--- lab3/src/scripts.js
+++ lab3/src/scripts.js
@@ -1,12 +1,44 @@
 
+var uniqueId = function() {
+	var date = Date.now();
+	var random = Math.random() * Math.random();
+
+	return Math.floor(date * random).toString();
+};
+
+var theMessage = function(theText, theName, theEdit, theDelete) {
+	return {
+		description:theText,
+		user:theName,
+		theEdit: !!theEdit,
+		theDelete: !!theDelete,
+        id: uniqueId()
+	};
+};
+
+var messageList = [];
+
 function run() {
     var appContainer = document.getElementsByClassName('main')[0];
  
  
     appContainer.addEventListener('click', delegateEvent);
- 
+
+    var allMessages = restore() || [];
+
+
+    createAllMessages(allMessages);
+
+
 }
- 
+
+function createAllMessages(allMessages) {
+	for(var i = 0; i < allMessages.length; i++){
+		addTodo(allMessages[i]);
+		}
+}
+
+
 function delegateEvent(evtObj) {
     if (evtObj.type === 'click' && evtObj.target.classList.contains('Right_Block_Button_Enter')) {
         onAddButtonClick(evtObj);
@@ -20,8 +52,9 @@ function delegateEvent(evtObj) {
     if (evtObj.type === 'click' && evtObj.target.classList.contains('delete_pic_button')) {
         deleteTextMessage(evtObj);
     }
- 
 }
+
+
  
 function EnterNameFunction(){
     var _name = document.getElementById("Enter_name").value;
@@ -45,82 +78,100 @@ function getMessage() {
     message = document.getElementById("Write-message").value;
     return message;
 }
- 
- 
+
  
 function editTextMessage(event){
     var usernameM = event.target.parentNode.parentNode.firstChild.innerHTML;
+    var isDeleteM = event.target.parentNode.parentNode.getAttribute('isDelete');
     if (usernameM != name) {
         alert("You are not allowed to edit messages of " + usernameM);
-    return;
+        return;
     }
- 
-    if(event.target.parentNode.nextSibling.nextSibling.innerHTML != 'Message is deleted'){
+    if(isDeleteM == 'false'){
         var textM = event.target.parentNode.nextSibling.nextSibling.innerHTML;
         var _message = prompt("Edit text message: ", textM);
-        var usernameM = event.target.parentNode.previousSibling.previousSibling.previousSibling.innerHTML;
-        if ((_message != null) && (name == usernameM)) {
+        if (_message != null) {
             event.target.parentNode.nextSibling.nextSibling.innerHTML = _message + ' (Edited)';
             event.target.parentNode.previousSibling.previousSibling.setAttribute('checked', 'checked');
+            swapEditAttribute(event.target.parentNode.parentNode.getAttribute('message-id'), _message);
         } else{
             alert('Error!');
         }
         } else{
         alert('Error! Message is deleted!!!');
     }
- 
+}
+
+function swapEditAttribute(valueId, newText) {
+    for (var i = 0; i < messageList.length; i++) {
+        if (messageList[i].id == valueId) {
+            messageList[i].theEdit = true;
+            messageList[i].description = newText + ' (Edited)';
+            store(messageList);
+            return;
+        }
+    }
 }
  
  
 function deleteTextMessage(event){
     var usernameM = event.target.parentNode.parentNode.firstChild.innerHTML;
+    var isDeleteM = event.target.parentNode.parentNode.getAttribute('isDelete');
     if (usernameM != name) {
         alert("You are not allowed to remove messages of " + usernameM);
-    return;
+        return;
     }
-    var textM = event.target.parentNode.nextSibling.innerHTML;
-    var usernameM = event.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.innerHTML;
-    if((textM != 'Message is deleted') && (usernameM == name)) {
-        var isDelete = confirm("Do you really want to delete a message?");
-        if(isDelete) {
+    if(isDeleteM == 'false') {
+        var isDeleteAnswer = confirm("Do you really want to delete a message?");
+        if(isDeleteAnswer) {
             event.target.parentNode.nextSibling.innerHTML = 'Message is deleted';
+            event.target.parentNode.parentNode.setAttribute('isDelete', 'true');
+            swapDeleteAttribute(event.target.parentNode.parentNode.getAttribute('message-id'));
         }
     } else{
         alert('Error!');
     }
 }
+
+function swapDeleteAttribute(valueId) {
+    for (var i = 0; i < messageList.length; i++) {
+        if (messageList[i].id == valueId) {
+            messageList[i].theDelete = true;
+            messageList[i].description = 'Message is deleted';
+            store(messageList);
+            return;
+        }
+    }
+}
+
  
 function onAddButtonClick() {
     var todoText = getMessage();
-    addTodo(todoText);
+
+    if (!todoText) {
+        return;
+        }
+
+    var newMessage = theMessage(todoText, name, false, false);
+    addTodo(newMessage);
     todoText = '';
     document.getElementById("Write-message").value = '';
+
+    store(messageList);
 }
  
  
-function addTodo(value) {
-    if (!value) {
-    return;
-    }
- 
-    var item = createMessage(value);
+function addTodo(newMessage) {
+    var item = createMessage(newMessage);
     var items = document.getElementsByClassName('Read-message')[0];
+    messageList.push(newMessage);
     items.appendChild(item);
     items.scrollTop +=items.scrollHeight;
+
 }
- 
-function createItem(text) {
-    var divItem = document.createElement('div');
-    divItem.classList.add('message');
- 
-    divItem.appendChild(createMessage(text));
- 
- 
-    return divItem;
-}
- 
- 
-function createMessage(text) {
+
+
+function createMessage(newMessage) {
     var username = document.createElement('div');
     var message = document.createElement('div');
     var checkMessage = document.createElement('input');
@@ -129,6 +180,7 @@ function createMessage(text) {
     var button_img_delete = document.createElement('button');
     var img_edit = document.createElement('img');
     var img_delete = document.createElement('img');
+
  
     img_edit.classList.add('edit_pic_button');
     img_delete.classList.add('delete_pic_button');
@@ -142,17 +194,45 @@ function createMessage(text) {
     button_img_delete.classList.add('delete_message_button');
     checkMessage.setAttribute('type', 'checkbox');
     checkMessage.setAttribute('disabled', 'disabled');
-    username.appendChild(document.createTextNode(name));
-    textMessage.appendChild(document.createTextNode(text));
+    if (newMessage.theEdit) {
+        checkMessage.setAttribute('checked', 'checked');
+    }
+    username.appendChild(document.createTextNode(newMessage.user));
+    textMessage.appendChild(document.createTextNode(newMessage.description));
  
     button_img_edit.appendChild(img_edit);
     button_img_delete.appendChild(img_delete);
- 
+
+    message.setAttribute('message-id', newMessage.id);
+    message.setAttribute('isDelete', newMessage.theDelete);
     message.appendChild(username);
     message.appendChild(checkMessage);
-    message.appendChild(document.createTextNode(name));
+    message.appendChild(document.createTextNode(newMessage.user));
     message.appendChild(button_img_edit);
     message.appendChild(button_img_delete);
     message.appendChild(textMessage);
     return message;
 }
+
+function store(listToSave) {
+
+    //alert(JSON.stringify(listToSave, null, 2));
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	localStorage.setItem("CHATs messageList", JSON.stringify(listToSave));
+}
+
+function restore() {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	var item = localStorage.getItem("CHATs messageList");
+
+	return item && JSON.parse(item);
+}
+
