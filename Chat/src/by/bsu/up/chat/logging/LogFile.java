edecdf65a9e0bcfb37f9b32fe75e4b4d3a0d573d
Anastasia <anastasiameoww@gmail.com>
2016-04-05 22:57:48 +0300

    [lab6]

---
+++ Chat/src/by/bsu/up/chat/logging/LogFile.java
@@ -0,0 +1,13 @@
+package by.bsu.up.chat.logging;
+
+public class LogFile implements Logger {
+    @Override
+    public void info(String message) {
+
+    }
+
+    @Override
+    public void error(String message, Throwable e) {
+
+    }
+}
